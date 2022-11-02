f7af403ae2443cf24e934bb300e5a203f4ade5ac
AlenaGM <alena.guillaume4@gmail.com>
2022-11-02 19:45:12 +0100

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
-"use strict"
+"use strict";
 
-document.addEventListener("DOMContentLoaded", function(){
-    fetch("https://api.nasa.gov/planetary/apod?api_key=7l6USJJeCaSnkPLvHrVUAsKhFf4FBKt6GtB3HXyS")
-    .then(response => response.json())
-    .then(planet => {
-        console.log('hihihi');
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=IdfdYntEsRMtYfqEN41j6HnIMA4LPsEYcyN21y5x"
+  )
+    .then((response) => response.json())
+    .then((planet) => {
+      let apodContent = "7";
 
-        let apodContent = "7";
-
-        apodContent =
-            `<h2 class="planet__name">${planet.title}</h2>
+      apodContent = `<h2 class="planet__name">${planet.title}</h2>
                 <div class="planet__date">${planet.date}</div>
                 <img class="planet__image" src="${planet.url}" alt="astronomy picture"></img>
-                <div class="planet__text">${planet.explanation}</div>`
+                <div class="planet__text">${planet.explanation}</div>`;
 
-        document.getElementById("planet").innerHTML = apodContent;
-        document.getElementById("footer").innerHTML = `<div>©${planet.copyright}</div>`
+      document.getElementById("planet").innerHTML = apodContent;
+      document.getElementById(
+        "footer"
+      ).innerHTML = `<div>©${planet.copyright}</div>`;
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 });
-
-
